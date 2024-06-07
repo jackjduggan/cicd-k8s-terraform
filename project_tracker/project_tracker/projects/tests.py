339bec9733367437a27aa1a339942e08472195ca
jackjduggan <20094010@mail.wit.ie>
2024-06-07 22:28:14 +0100

    Added basic tests to test project creation

--- project_tracker/project_tracker/projects/tests.py
+++ project_tracker/project_tracker/projects/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Project
+from django.utils import timezone
 
-# Create your tests here.
+class ProjectModelTest(TestCase):
+
+    def setUp(self):
+        Project.objects.create(
+            name="Test Project", 
+            description="An Awesome Project",
+            start_date=timezone.now().date()
+            )
+
+    def test_project_creation(self):
+        project = Project.objects.get(name="Test Project")
+        self.assertEqual(project.description, "An Awesome Project")
